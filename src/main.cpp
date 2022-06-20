145fd970675e83a8813797d08867d18a887dc5a6
Arturo Guerra <ar2roguerra@gmail.com>
2022-06-19 22:30:23 -0500

    Moved address detection to the state machine

--- src/main.cpp
+++ src/main.cpp
@@ -37,15 +37,10 @@ int main() {
 
     IO io = IO(pio, sm);
     
-    //addrbus_t oldaddr = 0xFFFF;
-
     while(true) {
         addrbus_t address = io.address();
         data_t data = eeprom.read(address);        
         io.wdata(data);
         printf("A: 0x%04X D: 0x%02X\n", address, data);
-        //if (address != oldaddr) {
-        //    oldaddr = address; 
-       // }
     }
 }
